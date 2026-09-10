4f0c3fd5d6263ef0f7ffd0ae9ffa91bcfa2614f8
kingsleydellali-pixel <kingsleydellali@gmail.com>
2026-09-10 18:44:29 +0000

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,8 +3,8 @@
 module.exports = {
   BOT_NAME: 'ɢʟᴀᴅᴅʏ xᴅ', // ur bot name
   OWNER_NAME: 'ɢʟᴀᴅᴅʏ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263775953409', // add your phone number
+  OWNER_NUMBER: '233535502036', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
-  TIMEZONE: 'Africa/Harare' // ur timezone
+  TIMEZONE: 'Africa/Accra' // ur timezone
 };
